c6a51da84bf0d921e8188ddf25c47fe83521bd99
ashok <urashok15@gmail.com>
2024-11-04 22:06:08 +0530

    updaed

--- neetcode/duplicate.py
+++ neetcode/duplicate.py
@@ -1,11 +1,15 @@
 # https://neetcode.io/problems/duplicate-integer
 def hasDuplicate(nums):
-    visited = []
-    for x in nums:
-        if(x in visited):
-            return True
-        else:
-            visited.append(x)
-    return False
+    seen=[]
+    for x in (nums):
+      if x == x+1:
+        print("given set has duplicate numbers")
+      for x in range(len(nums)):
+         seen=nums(x)
+         for y in range(x+1,len(nums))
+         if (seen==num(y))
+         break
+          return true
+         else False
 print(hasDuplicate([1, 2, 3, 4]))
 # Time complexity: O(n), Space complexity: O(n)
